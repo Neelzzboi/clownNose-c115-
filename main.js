5e98496e28fb73e3ef48ee90346cdff9a5e6909c
Neelzzboi <85091341+Neelzzboi@users.noreply.github.com>
2023-11-10 17:07:36 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-function preload(){
+noseX = 0;
+noseY = 0;
 
-}
+
+function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');}
 
 function setup(){
     canvas = createCanvas(300,300);
@@ -22,11 +25,14 @@ function gotPoses(results){
         console.log(results);
         console.log("nose x = "+results[0].pose.nose.x);
         console.log("nose y = "+results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
     }
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function takeSnapshot(){
